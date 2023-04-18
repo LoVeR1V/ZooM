99e4d93a074821c42bac8085af6e18ae286415f1
Vova <s17102002s92@gmail.com>
2023-04-18 23:13:24 +0300

    added connection to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserEntity } from './modules/users/entities/user.entity';
 
 @Module({
-  imports: [],
+
+  
+  imports: [
+    TypeOrmModule.forRoot({
+      "type": "mysql",
+      "host": "localhost",
+      "port": 3306,
+      "username": "neepman",
+      "password": "1234",
+      "database": "mydb",
+      "entities": [UserEntity],
+      "synchronize": true
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
+  
 })
+
+
+
 export class AppModule {}
